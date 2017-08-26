d5daf04ce24c470c7fa448948f4a9ed2f9c734ee
littlewhiteYA <F74014041@mail.ncku.edu.tw>
2017-08-27 00:31:22 +0800

    simple fuzzy search

--- src/bot/utils/getQueryResult.js
+++ src/bot/utils/getQueryResult.js
@@ -1,18 +1,21 @@
 import * as videos from '../../models/videos';
 
 const getQueryResult = async (type, keyword = null, page = null) => {
-  if (type !== 'PPAV') {
-    const { total_count: totalCount, result } = await videos.getVideo(
-      type,
-      keyword,
-      page
-    );
-
-    return { totalCount, result };
+  if (type === 'PPAV') {
+    const { result } = await videos.getOneRandomVideo();
+    return result;
   }
 
-  const { result } = await videos.getOneRandomVideo();
-  return result;
+  let _keyword = keyword;
+  for (let i = 0; i <= 2; i += 1) {
+    // eslint-disable-next-line no-await-in-loop
+    const { totalCount, result } = await videos.getVideo(type, _keyword, page);
+    _keyword = _keyword.substring(0, _keyword.length - 1);
+
+    if (totalCount !== 0 || i === 2) {
+      return { totalCount, result };
+    }
+  }
 };
 
 export default getQueryResult;
