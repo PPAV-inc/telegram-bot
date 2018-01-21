3aaa403fb50aac9ade03d58a7b89d26bfc688b7b
littlewhiteYA <F74014041@mail.ncku.edu.tw>
2018-01-21 12:13:57 +0800

    let getHotVideos return something when developing

--- src/models/videos.js
+++ src/models/videos.js
@@ -81,30 +81,38 @@ const getNewVideos = async () => {
 const getHotVideos = async () => {
   const db = await getMongoDatabase();
 
-  const sevenDaysBefore = subDays(new Date(), 7);
-  let hotVideos = await db
-    .collection('logs')
-    .aggregate([
-      { $match: { createdAt: { $gte: sevenDaysBefore } } },
-      {
-        $group: {
-          _id: '$videoId',
-          videoId: { $first: '$videoId' },
-          count: { $sum: 1 },
+  let results;
+  if (process.env.NODE_ENV === 'development') {
+    results = await db
+      .collection('videos')
+      .aggregate([{ $sample: { size: 5 } }])
+      .toArray();
+  } else {
+    const sevenDaysBefore = subDays(new Date(), 7);
+    let hotVideos = await db
+      .collection('logs')
+      .aggregate([
+        { $match: { createdAt: { $gte: sevenDaysBefore } } },
+        {
+          $group: {
+            _id: '$videoId',
+            videoId: { $first: '$videoId' },
+            count: { $sum: 1 },
+          },
         },
-      },
-      { $sort: { count: -1 } },
-      { $limit: 100 },
-      { $sample: { size: 5 } },
-    ])
-    .toArray();
+        { $sort: { count: -1 } },
+        { $limit: 100 },
+        { $sample: { size: 5 } },
+      ])
+      .toArray();
 
-  hotVideos = hotVideos.map(video => ObjectId(video.videoId));
+    hotVideos = hotVideos.map(video => ObjectId(video.videoId));
 
-  const results = await db
-    .collection('videos')
-    .find({ _id: { $in: hotVideos } })
-    .toArray();
+    results = await db
+      .collection('videos')
+      .find({ _id: { $in: hotVideos } })
+      .toArray();
+  }
 
   results.forEach(eachResult => {
     // eslint-disable-next-line no-param-reassign
